2a849767de0db6578318cd51ffa54ecba52aab21
sameerthecoder94 <sameerthecoder@gmail.com>
2022-04-06 17:44:32 -0400

    react portal implementation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React Portal
+
+> It is a first-class way to render child components into a DOM node outside of the parent DOM hierarchy.
--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ModalPortal = (props) => {
+  return ReactDOM.createPortal(
+    <div className='modal-portal'>
+      <div className='modal-content'>{props.children}</div>
+    </div>,
+    document.getElementById('portal')
+  );
+};
+
 function App() {
+  const [state, setState] = React.useState(false);
+
+  const openModal = () => {
+    setState(true);
+  };
+
+  const closeModal = () => {
+    setState(false);
+  };
+
   return (
     <div>
       <h1>React Portal</h1>
+      <button onClick={openModal}>Open Modal</button>
+      {state && (
+        <ModalPortal>
+          <p>This text is opened in a Portal.</p>
+          <button onClick={closeModal}>Close Modal</button>
+        </ModalPortal>
+      )}
     </div>
   );
 }
